0c4443f0d2b3bddc9950a0135c642fe8ac1bcc79
nesco <nesco_sochi@mail.ru>
2011-02-25 14:15:23 +0000

    add: Added Unit MSCryptoAPI

---
+++ code/MSCryptoAPI.pas
@@ -0,0 +1,145 @@
+unit MSCryptoAPI;
+
+interface
+
+uses
+  Windows;
+
+type
+  HCRYPTPROV  = Cardinal;
+  HCRYPTKEY   = Cardinal;
+  ALG_ID      = Cardinal;
+  PHCRYPTPROV = ^HCRYPTPROV;
+  PHCRYPTKEY  = ^HCRYPTKEY;
+  HCRYPTHASH  = Cardinal;
+  PHCRYPTHASH = ^HCRYPTHASH;
+  PLongWord   = ^LongWord;
+  
+const
+  ADVAPI32            = 'advapi32.dll';
+
+  RSA384BIT_KEY       = $01800000;
+  RSA512BIT_KEY       = $02000000;
+  RSA1024BIT_KEY      = $04000000;
+  RSA2048BIT_KEY      = $08000000;
+  RSA4096BIT_KEY      = $10000000;
+//  RSA8192BIT_KEY      = $20000000;
+//  RSA16384BIT_KEY     = $40000000;
+
+  // exported key blob definitions
+  SIMPLEBLOB          = $1;
+  PUBLICKEYBLOB       = $6;
+  PRIVATEKEYBLOB      = $7;
+  PLAINTEXTKEYBLOB    = $8;
+
+  AT_KEYEXCHANGE      = 1;
+  AT_SIGNATURE        = 2;
+
+  CRYPT_USERDATA      = 1;
+
+  CRYPT_BLOB_VER3     = $00000080;
+
+  CALG_MD2            = 32769;
+  CALG_MD4            = 32770;
+  CALG_MD5            = 32771;
+  CALG_SHA            = 32772;
+  CALG_SHA_1          = 32772;
+  CALG_SHA_256        = 32780;
+  CALG_SHA_384        = 32781;
+  CALG_SHA_512        = 32782;
+  CALG_RC2            = 26114;
+  CALG_RC4            = 26625;
+  CALG_RC5            = 26125;
+  CALG_DES            = 26113;
+  CALG_3DES_112       = 26121;
+  CALG_3DES           = 26115;
+  CALG_DESX           = 26116;
+  CALG_AES            = 26129;
+  CALG_AES_128        = 26126;
+  CALG_AES_192        = 26127;
+  CALG_AES_256        = 26128;
+  CALG_CYLINK_MEK     = 26124;
+  CALG_RSA_KEYX       = 41984;
+  CALG_RSA_SIGN       = 9216;
+
+  HP_ALGID            = $0001; // Hash algorithm
+  HP_HASHVAL          = $0002; // Hash value
+  HP_HASHSIZE         = $0004; // Hash value size
+
+  // dwFlags definitions for CryptAcquireContext
+  CRYPT_VERIFYCONTEXT  = $F0000000;
+  CRYPT_NEWKEYSET      = $00000008;
+  CRYPT_DELETEKEYSET   = $00000010;
+  CRYPT_MACHINE_KEYSET = $00000020;
+
+  // dwFlag definitions for CryptGenKey
+  CRYPT_EXPORTABLE     = $00000001;
+  CRYPT_USER_PROTECTED = $00000002;
+  CRYPT_CREATE_SALT    = $00000004;
+  CRYPT_UPDATE_KEY     = $00000008;
+  CRYPT_NO_SALT        = $00000010;
+  CRYPT_PREGEN         = $00000040;
+  CRYPT_RECIPIENT      = $00000010;
+  CRYPT_INITIATOR      = $00000040;
+  CRYPT_ONLINE         = $00000080;
+  CRYPT_SF             = $00000100;
+  CRYPT_CREATE_IV      = $00000200;
+  CRYPT_KEK            = $00000400;
+  CRYPT_DATA_KEY       = $00000800;
+
+  // dwFlags definitions for CryptDeriveKey
+  CRYPT_SERVER         = $00000400;
+
+  PROV_RSA_FULL         = 1;
+  PROV_RSA_SIG          = 2;
+  PROV_DSS              = 3;
+  PROV_FORTEZZA         = 4;
+  PROV_MS_EXCHANGE      = 5;
+  PROV_SSL              = 6;
+  PROV_RSA_SCHANNEL     = 12;
+  PROV_DSS_DH           = 13;
+  PROV_EC_ECDSA_SIG     = 14;
+  PROV_EC_ECNRA_SIG     = 15;
+  PROV_EC_ECDSA_FULL    = 16;
+  PROV_EC_ECNRA_FULL    = 17;
+  PROV_DH_SCHANNEL      = 18;
+  PROV_SPYRUS_LYNKS     = 20;
+  PROV_RNG              = 21;
+  PROV_INTEL_SEC        = 22;
+  PROV_REPLACE_OWF      = 23;
+  PROV_RSA_AES          = 24;
+
+  MS_DEF_DH_SCHANNEL_PROV   = 'Microsoft DH Schannel Cryptographic Provider';
+  MS_DEF_DSS_DH_PROV        = 'Microsoft Base DSS and Diffie-Hellman Cryptographic Provider';
+  MS_DEF_DSS_PROV           = 'Microsoft Base DSS Cryptographic Provider';
+  MS_DEF_PROV               = 'Microsoft Base Cryptographic Provider v1.0';
+  MS_DEF_RSA_SCHANNEL_PROV  = 'Microsoft RSA Schannel Cryptographic Provider';
+  MS_DEF_RSA_SIG_PROV       = 'Microsoft RSA Signature Cryptographic Provider';
+  MS_ENH_DSS_DH_PROV        = 'Microsoft Enhanced DSS and Diffie-Hellman Cryptographic Provider';
+  MS_ENH_RSA_AES_PROV       = 'Microsoft Enhanced RSA and AES Cryptographic Provider';
+  MS_ENHANCED_PROV          = 'Microsoft Enhanced Cryptographic Provider v1.0';
+  MS_SCARD_PROV             = 'Microsoft Base Smart Card Crypto Provider';
+  MS_STRONG_PROV            = 'Microsoft Strong Cryptographic Provider';
+
+function CryptAcquireContext(Prov: PHCRYPTPROV; Container: PChar; Provider: PChar; ProvType: LongWord; Flags: LongWord): LongBool; stdcall; external ADVAPI32 name 'CryptAcquireContextA';
+function CryptGenKey(hProv: HCRYPTPROV; Algid: ALG_ID; dwFlags: LongWord; phKey: PHCRYPTKEY): LongBool; stdcall; external ADVAPI32 name 'CryptGenKey';
+function CryptGetUserKey(hProv: HCRYPTPROV; dwKeySpec: LongWord; phUserKey: PHCRYPTKEY): LongBool; stdcall; external ADVAPI32 name 'CryptGetUserKey';
+function CryptDeriveKey(Prov: HCRYPTPROV; Algid: ALG_ID; BaseData: HCRYPTHASH; Flags: LongWord; Key: PHCRYPTKEY): LongBool; stdcall; external ADVAPI32 name 'CryptDeriveKey';
+function CryptExportKey(hKey: HCRYPTKEY; hExpKey: HCRYPTKEY; dwBlobType: LongWord; dwFlags: LongWord; pbData: PChar; pdwDataLen: PLongWord): LongBool; stdcall; external ADVAPI32 name 'CryptExportKey';
+function CryptImportKey(hProv: HCRYPTPROV; pbData: PChar; dwDataLen: LongWord; hPubKey: HCRYPTKEY; dwFlags: LongWord; phKey: PHCRYPTKEY):LongBool; stdcall; external ADVAPI32 name 'CryptImportKey';
+
+function CryptEncrypt(Key: HCRYPTKEY; Hash: HCRYPTHASH; Final: LongBool; Flags: LongWord; Data: PChar; Len: PLongWord; BufLen: LongWord): LongBool;stdcall;external ADVAPI32 name 'CryptEncrypt';
+function CryptDecrypt(Key: HCRYPTKEY; Hash: HCRYPTHASH; Final: LongBool; Flags: LongWord; Data: PChar; Len: PLongWord): LongBool; stdcall; external ADVAPI32 name 'CryptDecrypt';
+
+function CryptCreateHash(Prov: HCRYPTPROV; Algid: ALG_ID; Key: HCRYPTKEY; Flags: LongWord; Hash: PHCRYPTHASH): LongBool; stdcall; external ADVAPI32 name 'CryptCreateHash';
+function CryptHashData(Hash: HCRYPTHASH; Data: PChar; DataLen: LongWord; Flags: LongWord): LongBool; stdcall; external ADVAPI32 name 'CryptHashData';
+function CryptGetHashParam(hHash: HCRYPTHASH; dwParam: LongWord; pbData: PChar; pdwDataLen: PLongWord; dwFlags: LongWord): LongBool; stdcall; external ADVAPI32 name 'CryptGetHashParam';
+function CryptSignHash(hHash: HCRYPTHASH; dwKeySpec: LongWord; sDescription: PAnsiChar; dwFlags: LongWord; pbSignature: PChar; pdwSigLen: PLongWord): LongBool; stdcall; external ADVAPI32 name 'CryptSignHashA';
+function CryptVerifySignature(hHash: HCRYPTHASH; const pbSignature: PChar; dwSigLen: LongWord; hPubKey: HCRYPTKEY; sDescription: PAnsiChar; dwFlags: LongWord): LongBool; stdcall; external ADVAPI32 name 'CryptVerifySignatureA';
+
+function CryptReleaseContext(hProv: HCRYPTPROV; dwFlags: LongWord): LongBool; stdcall; external ADVAPI32 name 'CryptReleaseContext';
+function CryptDestroyHash(hHash: HCRYPTHASH): LongBool; stdcall; external ADVAPI32 name 'CryptDestroyHash';
+function CryptDestroyKey(hKey: HCRYPTKEY): LongBool; stdcall; external ADVAPI32 name 'CryptDestroyKey';
+
+implementation
+end.
--- code/hiCryptography.pas
+++ code/hiCryptography.pas
@@ -2,22 +2,12 @@ unit hiCryptography;
 
 interface
 
-uses Windows,Kol,Share,Debug;
+uses Windows, Kol, Share, Debug, MSCryptoAPI;
 
 const
   CRYPT_MODE   = 0;
   DECRYPT_MODE = 1;
   
-type
-  HCRYPTPROV  = Cardinal;
-  HCRYPTKEY   = Cardinal;
-  ALG_ID      = Cardinal;
-  PHCRYPTPROV = ^HCRYPTPROV;
-  PHCRYPTKEY  = ^HCRYPTKEY;
-  HCRYPTHASH  = Cardinal;
-  PHCRYPTHASH = ^HCRYPTHASH;
-  PLongWord   = ^LongWord;
-
 type
  TThreadRec = record
    handle:cardinal;
@@ -49,23 +39,23 @@ type
     procedure _work_doCrypt0(var _Data:TData; Index:word);  // XOR
     procedure _work_doCrypt1(var _Data:TData; Index:word);  // RC2
     procedure _work_doCrypt2(var _Data:TData; Index:word);  // RC4
-    procedure _work_doCrypt3(var _Data:TData; Index:word);  // DES_56
-    procedure _work_doCrypt4(var _Data:TData; Index:word);  // 3DES_112
-    procedure _work_doCrypt5(var _Data:TData; Index:word);  // 3DES_168                    
-    procedure _work_doCrypt6(var _Data:TData; Index:word);  // AES_128    
-    procedure _work_doCrypt7(var _Data:TData; Index:word);  // AES_192
-    procedure _work_doCrypt8(var _Data:TData; Index:word);  // AES_256        
+    procedure _work_doCrypt3(var _Data:TData; Index:word);  // DES56
+    procedure _work_doCrypt4(var _Data:TData; Index:word);  // 3DES112
+    procedure _work_doCrypt5(var _Data:TData; Index:word);  // 3DES168                    
+    procedure _work_doCrypt6(var _Data:TData; Index:word);  // AES128    
+    procedure _work_doCrypt7(var _Data:TData; Index:word);  // AES192
+    procedure _work_doCrypt8(var _Data:TData; Index:word);  // AES256        
     procedure _work_doCrypt9(var _Data:TData; Index:word);  // CYLINK_MEK        
 
     procedure _work_doDeCrypt0(var _Data:TData; Index:word);  // XOR
     procedure _work_doDeCrypt1(var _Data:TData; Index:word);  // RC2
     procedure _work_doDeCrypt2(var _Data:TData; Index:word);  // RC4
-    procedure _work_doDeCrypt3(var _Data:TData; Index:word);  // DES_56
-    procedure _work_doDeCrypt4(var _Data:TData; Index:word);  // 3DES_112
-    procedure _work_doDeCrypt5(var _Data:TData; Index:word);  // 3DES_168                    
-    procedure _work_doDeCrypt6(var _Data:TData; Index:word);  // AES_128    
-    procedure _work_doDeCrypt7(var _Data:TData; Index:word);  // AES_192
-    procedure _work_doDeCrypt8(var _Data:TData; Index:word);  // AES_256        
+    procedure _work_doDeCrypt3(var _Data:TData; Index:word);  // DES56
+    procedure _work_doDeCrypt4(var _Data:TData; Index:word);  // 3DES112
+    procedure _work_doDeCrypt5(var _Data:TData; Index:word);  // 3DES168                    
+    procedure _work_doDeCrypt6(var _Data:TData; Index:word);  // AES128    
+    procedure _work_doDeCrypt7(var _Data:TData; Index:word);  // AES192
+    procedure _work_doDeCrypt8(var _Data:TData; Index:word);  // AES256        
     procedure _work_doDeCrypt9(var _Data:TData; Index:word);  // CYLINK_MEK        
 
 
@@ -187,67 +177,6 @@ end;
 
 // ============================================ MS CryptoAPI ==========================================
 
-const
-  ADVAPI32            = 'advapi32.dll';
-  CRYPT_VERIFYCONTEXT = $F0000000;
-  CALG_MD2            = 32769;
-  CALG_MD4            = 32770;
-  CALG_MD5            = 32771;
-  CALG_SHA            = 32772;
-  CALG_RC2            = 26114;
-  CALG_RC4            = 26625;
-  CALG_RC5            = 26125;
-  CALG_DES            = 26113;
-  CALG_3DES_112       = 26121;
-  CALG_3DES           = 26115;
-  CALG_DESX           = 26116;
-  CALG_AES            = 26129;
-  CALG_AES_128        = 26126;
-  CALG_AES_192        = 26127;
-  CALG_AES_256        = 26128;  
-  CALG_CYLINK_MEK     = 26124;
-
-  PROV_RSA_FULL         = 1;
-  PROV_RSA_SIG          = 2;
-  PROV_DSS              = 3;
-  PROV_FORTEZZA         = 4;
-  PROV_MS_EXCHANGE      = 5;
-  PROV_SSL              = 6;
-  PROV_RSA_SCHANNEL     = 12;
-  PROV_DSS_DH           = 13;
-  PROV_EC_ECDSA_SIG     = 14;
-  PROV_EC_ECNRA_SIG     = 15;
-  PROV_EC_ECDSA_FULL    = 16;
-  PROV_EC_ECNRA_FULL    = 17;
-  PROV_DH_SCHANNEL      = 18;
-  PROV_SPYRUS_LYNKS     = 20;
-  PROV_RNG              = 21;
-  PROV_INTEL_SEC        = 22;
-  PROV_REPLACE_OWF      = 23;
-  PROV_RSA_AES          = 24;
-
-  MS_DEF_DH_SCHANNEL_PROV   = 'Microsoft DH Schannel Cryptographic Provider'; 
-  MS_DEF_DSS_DH_PROV        = 'Microsoft Base DSS and Diffie-Hellman Cryptographic Provider'; 
-  MS_DEF_DSS_PROV           = 'Microsoft Base DSS Cryptographic Provider'; 
-  MS_DEF_PROV               = 'Microsoft Base Cryptographic Provider v1.0'; 
-  MS_DEF_RSA_SCHANNEL_PROV  = 'Microsoft RSA Schannel Cryptographic Provider'; 
-  MS_DEF_RSA_SIG_PROV       = 'Microsoft RSA Signature Cryptographic Provider'; 
-  MS_ENH_DSS_DH_PROV        = 'Microsoft Enhanced DSS and Diffie-Hellman Cryptographic Provider'; 
-  MS_ENH_RSA_AES_PROV       = 'Microsoft Enhanced RSA and AES Cryptographic Provider'; 
-  MS_ENHANCED_PROV          = 'Microsoft Enhanced Cryptographic Provider v1.0'; 
-  MS_SCARD_PROV             = 'Microsoft Base Smart Card Crypto Provider'; 
-  MS_STRONG_PROV            = 'Microsoft Strong Cryptographic Provider';
- 
-function CryptReleaseContext(hProv: HCRYPTPROV; dwFlags: LongWord): LongBool; stdcall; external ADVAPI32 name 'CryptReleaseContext';
-function CryptAcquireContext(Prov: PHCRYPTPROV; Container: PChar; Provider: PChar; ProvType: LongWord; Flags: LongWord): LongBool; stdcall; external ADVAPI32 name 'CryptAcquireContextA';
-function CryptEncrypt(Key: HCRYPTKEY; Hash: HCRYPTHASH; Final: LongBool; Flags: LongWord; Data: PChar; Len: PLongWord; BufLen: LongWord): LongBool;stdcall;external ADVAPI32 name 'CryptEncrypt';
-function CryptDecrypt(Key: HCRYPTKEY; Hash: HCRYPTHASH; Final: LongBool; Flags: LongWord; Data: PChar; Len: PLongWord): LongBool; stdcall; external ADVAPI32 name 'CryptDecrypt';
-function CryptCreateHash(Prov: HCRYPTPROV; Algid: ALG_ID; Key:HCRYPTKEY; Flags: LongInt; Hash: PHCRYPTHASH): LongBool; stdcall; external ADVAPI32 name 'CryptCreateHash';
-function CryptHashData(Hash: HCRYPTHASH; Data: PChar; DataLen: LongInt; Flags: LongInt): LongBool; stdcall; external ADVAPI32 name 'CryptHashData';
-function CryptDeriveKey(Prov: HCRYPTPROV; Algid: ALG_ID; BaseData: HCRYPTHASH; Flags: LongInt; Key: PHCRYPTKEY): LongBool; stdcall; external ADVAPI32 name 'CryptDeriveKey';
-function CryptDestroyHash(hHash: HCRYPTHASH): LongBool; stdcall; external ADVAPI32 name 'CryptDestroyHash';
-function CryptDestroyKey(hKey: HCRYPTKEY): LongBool; stdcall; external ADVAPI32 name 'CryptDestroyKey';
-
 procedure THICryptography.InitPass;
 var
   hash: HCRYPTHASH;
@@ -318,32 +247,32 @@ begin
   Crypt_Decrypt_MS_Prov(_Data, CALG_RC4, CRYPT_MODE, MS_ENHANCED_PROV, PROV_RSA_FULL);  
 end;
 
-procedure THICryptography._work_doCrypt3; // DES_56
+procedure THICryptography._work_doCrypt3; // DES56
 begin
   Crypt_Decrypt_MS_Prov(_Data, CALG_DES, CRYPT_MODE, MS_ENHANCED_PROV, PROV_RSA_FULL);
 end;
 
-procedure THICryptography._work_doCrypt4; // 3DES_112
+procedure THICryptography._work_doCrypt4; // 3DES112
 begin
   Crypt_Decrypt_MS_Prov(_Data, CALG_3DES_112, CRYPT_MODE, MS_ENHANCED_PROV, PROV_RSA_FULL);
 end;
 
-procedure THICryptography._work_doCrypt5; // 3DES_168
+procedure THICryptography._work_doCrypt5; // 3DES168
 begin
   Crypt_Decrypt_MS_Prov(_Data, CALG_3DES, CRYPT_MODE, MS_ENHANCED_PROV, PROV_RSA_FULL);
 end;
 
-procedure THICryptography._work_doCrypt6; // AES_128
+procedure THICryptography._work_doCrypt6; // AES128
 begin
   Crypt_Decrypt_MS_Prov(_Data, CALG_AES_128, CRYPT_MODE, MS_ENH_RSA_AES_PROV, PROV_RSA_AES);
 end;
 
-procedure THICryptography._work_doCrypt7; // AES_192
+procedure THICryptography._work_doCrypt7; // AES192
 begin
   Crypt_Decrypt_MS_Prov(_Data, CALG_AES_192, CRYPT_MODE, MS_ENH_RSA_AES_PROV, PROV_RSA_AES);
 end;
 
-procedure THICryptography._work_doCrypt8; // AES_256
+procedure THICryptography._work_doCrypt8; // AES256
 begin
   Crypt_Decrypt_MS_Prov(_Data, CALG_AES_256, CRYPT_MODE, MS_ENH_RSA_AES_PROV, PROV_RSA_AES);
 end;
@@ -365,32 +294,32 @@ begin
   Crypt_Decrypt_MS_Prov(_Data, CALG_RC4, DECRYPT_MODE, MS_ENHANCED_PROV, PROV_RSA_FULL);  
 end;
 
-procedure THICryptography._work_doDeCrypt3; // DES_56
+procedure THICryptography._work_doDeCrypt3; // DES56
 begin
   Crypt_Decrypt_MS_Prov(_Data, CALG_DES, DECRYPT_MODE, MS_ENHANCED_PROV, PROV_RSA_FULL);
 end;
 
-procedure THICryptography._work_doDeCrypt4;
+procedure THICryptography._work_doDeCrypt4; // 3DES112
 begin
-  Crypt_Decrypt_MS_Prov(_Data, CALG_3DES_112, DECRYPT_MODE, MS_ENHANCED_PROV, PROV_RSA_FULL); // 3DES_112
+  Crypt_Decrypt_MS_Prov(_Data, CALG_3DES_112, DECRYPT_MODE, MS_ENHANCED_PROV, PROV_RSA_FULL);
 end;
 
-procedure THICryptography._work_doDeCrypt5;
+procedure THICryptography._work_doDeCrypt5; // 3DES168
 begin
-  Crypt_Decrypt_MS_Prov(_Data, CALG_3DES, DECRYPT_MODE, MS_ENHANCED_PROV, PROV_RSA_FULL); // 3DES_168
+  Crypt_Decrypt_MS_Prov(_Data, CALG_3DES, DECRYPT_MODE, MS_ENHANCED_PROV, PROV_RSA_FULL);
 end;
 
-procedure THICryptography._work_doDeCrypt6; // AES_128
+procedure THICryptography._work_doDeCrypt6; // AES128
 begin
   Crypt_Decrypt_MS_Prov(_Data, CALG_AES_128, DECRYPT_MODE, MS_ENH_RSA_AES_PROV, PROV_RSA_AES);
 end;
 
-procedure THICryptography._work_doDeCrypt7; // AES_192
+procedure THICryptography._work_doDeCrypt7; // AES192
 begin
   Crypt_Decrypt_MS_Prov(_Data, CALG_AES_192, DECRYPT_MODE, MS_ENH_RSA_AES_PROV, PROV_RSA_AES);
 end;
 
-procedure THICryptography._work_doDeCrypt8; // AES_256
+procedure THICryptography._work_doDeCrypt8; // AES256
 begin
   Crypt_Decrypt_MS_Prov(_Data, CALG_AES_256, DECRYPT_MODE, MS_ENH_RSA_AES_PROV, PROV_RSA_AES);
 end;
