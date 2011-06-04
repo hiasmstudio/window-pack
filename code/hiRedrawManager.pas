19fdf7eb4e6a094a5e8b39abb1c5b0758614b7d4
nesco <nesco_sochi@mail.ru>
2011-06-04 21:22:31 +0000

    add: Methods doBeginUpdate and doEndUpdate

--- code/hiRedrawManager.pas
+++ code/hiRedrawManager.pas
@@ -11,6 +11,8 @@ type
      _prop_EraseMode: boolean;
      _prop_ControlManager:IControlManager;
      procedure _work_doRedraw(var _Data:TData; Index:word);
+     procedure _work_doBeginUpdate(var _Data: TData; Index: word);
+     procedure _work_doEndUpdate(var _Data: TData; Index: word);
   end;
 
 implementation
@@ -22,6 +24,24 @@ begin
   if not Assigned(_prop_ControlManager) then exit;
   sControl := _prop_ControlManager.ctrlpoint;
   InvalidateRect(sControl.Handle, nil, _prop_EraseMode);
+end;
+
+procedure THIRedrawManager._work_doBeginUpdate;
+var
+  sControl: PControl;
+begin
+  if not Assigned(_prop_ControlManager) then exit;
+  sControl := _prop_ControlManager.ctrlpoint;
+  sControl.BeginUpdate;
+end;
+
+procedure THIRedrawManager._work_doEndUpdate;
+var
+  sControl: PControl;
+begin
+  if not Assigned(_prop_ControlManager) then exit;
+  sControl := _prop_ControlManager.ctrlpoint;
+  sControl.EndUpdate;
 end;
 
 end.
