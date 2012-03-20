4c4d72a1d590d73c6eac343da23cb53061191cd9
nesco <nesco_sochi@mail.ru>
2012-03-20 08:50:23 +0000

    fix: Corrected Code

--- code/hiRecord.pas
+++ code/hiRecord.pas
@@ -22,6 +22,7 @@ type
     _prop_Speed:integer;
 
     _data_Filename:THI_Event;
+    _event_onError:THI_event;
     
     procedure _work_doRecord(var _Data:TData; Index:word);
     procedure _work_doSave(var _Data:TData; Index:word);
@@ -30,25 +31,39 @@ type
     procedure _work_doClose(var _Data:TData; Index:word);
     procedure _var_isOpen(var _Data:TData; Index:word);
     procedure _var_isRun(var _Data:TData; Index:word);
+
+    procedure _work_doBits(var _Data:TData; Index:word);
+    procedure _work_doStereo(var _Data:TData; Index:word);
+    procedure _work_doSpeed(var _Data:TData; Index:word);        
+
   end;
 
 implementation
 
 
 procedure THIRecord.open;
+var
+  Res: integer;
 begin
-   if not isOpen then
+  if not isOpen then
+  begin
+    mciSendString('OPEN NEW TYPE WAVEAUDIO ALIAS dlma', nil, 0, 0);
+    Res := mciSendString(PChar(
+      'SET dlma time format ms format tag pcm'
+      + ' bitspersample ' + int2str(_prop_Bits)       
+      + ' channels ' + int2str(_prop_Stereo)
+      + ' samplespersec ' + int2str(_prop_Speed)
+      + ' alignment ' + int2str(round(_prop_Bits * _prop_Stereo / 8))
+      + ' bytespersec ' + int2str(round(_prop_Bits * _prop_Stereo * _prop_Speed / 8))),
+    nil, 0, 0);
+    if Res <> 0 then
     begin
-      mciSendString('OPEN NEW TYPE WAVEAUDIO ALIAS dlma', nil, 0, 0);
-      mciSendString(PChar('SET dlma TIME FORMAT MS ' +
-       'BITSPERSAMPLE ' + int2str(_prop_Bits) +
-       'CHANNELS ' + int2str(_prop_Stereo) +
-       'SAMPLESPERSEC ' + int2str(_prop_Speed) +
-       'BYTESPERSEC ' + int2str(round(_prop_Bits*_prop_Stereo*_prop_Speed/8))),
-        nil, 0, 0);
-   end;
-   isOpen:=true;
-   isRun:=true;
+      mciSendString('close dlma ', @s, 255, 0);
+      _hi_onEvent(_event_onError);
+    end;
+  end;
+  isOpen:=true;
+  isRun:=true;
 end;
 
 procedure THIRecord._work_doRecord;
@@ -107,5 +122,21 @@ procedure THIRecord._var_isRun;
 begin
     dtInteger(_Data,Integer(isRun));
 end;
+
+procedure THIRecord._work_doBits(var _Data:TData; Index:word);
+begin
+  _prop_Bits := ToInteger(_Data);
+end;
+
+procedure THIRecord._work_doStereo(var _Data:TData; Index:word);
+begin
+  _prop_Stereo := ToInteger(_Data);
+end;
+
+procedure THIRecord._work_doSpeed(var _Data:TData; Index:word);        
+begin
+  _prop_Speed := ToInteger(_Data);
+end;
+
 
 end.
