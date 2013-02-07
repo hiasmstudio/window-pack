736080784f9b6f9bca2a5c196fcfb8e996f1a2aa
nesco <nesco_sochi@mail.ru>
2013-02-07 12:31:54 +0000

    add: Method doPortIsOpen

--- code/hiHost.pas
+++ code/hiHost.pas
@@ -7,37 +7,40 @@ uses Kol,Share,Debug,Win,{Windows,}winsock,{err,}ShellApi;
 type
   THIHost = class(TDebug)
    private
-
    public
-
      _event_onHostByIP : THI_Event;
      _event_onIPByHost : THI_Event;
+     _event_onPortIsOpen : THI_Event;
      _data_IP : THI_Event;
      _data_HostName : THI_Event;
+     _data_Port : THI_Event;     
     
      procedure _work_doHostByIP(var _Data:TData; Index:word);
      procedure _work_doIPByHost(var _Data:TData; Index:word);
+     procedure _work_doPortIsOpen(var _Data:TData; Index:word);
      procedure _var_LoacalHostName(var _Data:TData; Index:word);
      procedure _var_LoacalIP(var _Data:TData; Index:word);
      function uGetLoacalHostName:string ; 
      function uHost2IP(HostName:string): String; 
-     function uIP2Host(IPAddr : String): String; 
+     function uIP2Host(IPAddr : String): String;
+     function PortTCP_IsOpen(ipAddressStr: AnsiString; dwPort: Word) : boolean;      
   end;
 
 implementation
 
 procedure THIHost._work_doHostByIP;
-var   ip: string;   
 begin
-   ip := ReadString(_Data,_data_IP);
-  _hi_OnEvent(_event_onHostByIP,uIP2Host(uHost2IP(ip)));
+  _hi_OnEvent(_event_onHostByIP,uIP2Host(uHost2IP(ReadString(_Data,_data_IP))));
 end;
 
 procedure THIHost._work_doIPByHost;
-var   Host: string;   
 begin
-  Host := ReadString(_Data,_data_HostName,uGetLoacalHostName);
-  _hi_OnEvent(_event_onIPByHost,uHost2IP(Host));
+  _hi_OnEvent(_event_onIPByHost,uHost2IP(ReadString(_Data,_data_HostName,uGetLoacalHostName)));
+end;
+
+procedure THIHost._work_doPortIsOpen;
+begin
+  _hi_onEvent(_event_onPortIsOpen, ord(PortTCP_IsOpen(uHost2IP(ReadString(_Data,_data_HostName,uGetLoacalHostName)), ReadInteger(_Data, _data_Port))));
 end;
 
 procedure THIHost._var_LoacalHostName;
@@ -52,51 +55,80 @@ end;
 
 //////////////////////////////////////////////////////////////////////////////////////////////////
 function THIHost.uGetLoacalHostName:string ;
-var   Buffer: Array[0..63] of Char;
-      GInitData: TWSAData;
+var
+  Buffer: Array[0..63] of Char;
+  GInitData: TWSAData;
 begin
-   WSAStartup($101, GInitData);
-   Result := '';
-   winsock.GetHostName(Buffer, SizeOf(Buffer));
-   Result := Buffer;
-   WSACleanup;
+  WSAStartup($101, GInitData);
+  Result := '';
+  winsock.GetHostName(Buffer, SizeOf(Buffer));
+  Result := Buffer;
+  WSACleanup;
 end;
 ////////////////////////////////////////////////////////////////////////////////
 function THIHost.uHost2IP(HostName:string): String;
 type
-   TaPInAddr = Array[0..10] of PInAddr;
-   PaPInAddr = ^TaPInAddr;
-var   phe: PHostEnt;
-      pptr: PaPInAddr;
-      I: Integer;
-      GInitData: TWSAData;
+  TaPInAddr = Array[0..10] of PInAddr;
+  PaPInAddr = ^TaPInAddr;
+var
+  phe: PHostEnt;
+  pptr: PaPInAddr;
+  I: Integer;
+  GInitData: TWSAData;
 begin
-   Result := '';
-   WSAStartup($101, GInitData);
-   phe := GetHostByName(@HostName[1]);
-   if phe = nil then exit;
-   pPtr := PaPInAddr(phe^.h_addr_list);
-   I := 0;
-   while pPtr^[I] <> nil do begin
-      Result := inet_ntoa(pPtr^[I]^);
-      Inc(I);
-   end;
-   WSACleanup;
+  Result := '';
+  WSAStartup($101, GInitData);
+try
+  phe := GetHostByName(@HostName[1]);
+  if phe = nil then exit;
+  pPtr := PaPInAddr(phe^.h_addr_list);
+  I := 0;
+  while pPtr^[I] <> nil do
+  begin
+    Result := inet_ntoa(pPtr^[I]^);
+    Inc(I);
+  end;
+finally
+  WSACleanup;
+end;   
 end;
 ////////////////////////////////////////////////////////////////////////////////
 function THIHost.uIP2Host(IPAddr : String): String;
-var   SockAddrIn: TSockAddrIn;
-      HostEnt: PHostEnt;
-      WSAData: TWSAData;
+var
+  SockAddrIn: TSockAddrIn;
+  HostEnt: PHostEnt;
+  WSAData: TWSAData;
 begin
-   Result:='';
-   WSAStartup($101, WSAData);
-   SockAddrIn.sin_addr.s_addr:= inet_addr(PChar(IPAddr));
-   HostEnt:= GetHostByAddr(@SockAddrIn.sin_addr.S_addr, sizeof(in_addr), AF_INET);
-   if HostEnt = nil then exit;
-   Result := Hostent^.h_name;
-   WSACleanup;
+  Result:='';
+  WSAStartup($101, WSAData);
+try
+  SockAddrIn.sin_addr.s_addr:= inet_addr(PChar(IPAddr));
+  HostEnt:= GetHostByAddr(@SockAddrIn.sin_addr.S_addr, sizeof(in_addr), AF_INET);
+  if HostEnt = nil then exit;
+  Result := Hostent^.h_name;
+finally
+  WSACleanup;
+end;   
 end;
 ////////////////////////////////////////////////////////////////////////////////
-
+function THIHost.PortTCP_IsOpen(ipAddressStr: AnsiString; dwPort: Word) : boolean;
+var
+  client : sockaddr_in;
+  sock, ret : integer;
+  wsdata : WSAData;
+begin
+  Result := false;
+  ret := WSAStartup($101, wsdata);                              //initiates use of the Winsock DLL
+try
+  if ret <> 0 then exit;
+  client.sin_family      := AF_INET;                            //Set the protocol to use , in this case (IPv4)
+  client.sin_port        := htons(dwPort);                      //convert to TCP/IP network byte order (big-endian)
+  client.sin_addr.s_addr := inet_addr(PAnsiChar(ipAddressStr)); //convert to IN_ADDR structure
+  sock                   := socket(AF_INET, SOCK_STREAM, 0);    //creates a socket
+  Result := connect(sock, client, SizeOf(client)) = 0;          //establishes a connection to a specified socket
+finally
+  WSACleanup;
+end;
+end;
+////////////////////////////////////////////////////////////////////////////////
 end.
