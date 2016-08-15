f2068680428de92763cf0645148bc3465d752a42
nesco <nesco_sochi@mail.ru>
2016-08-15 07:48:07 +0000

    fix: Removed Blinking

--- code/hiTransparentManager.pas
+++ code/hiTransparentManager.pas
@@ -24,13 +24,19 @@ type
   THITransparentManager = class(TDebug)
    private
      tm:TITransparentManager;
+	 IsNotInitBmp: boolean;
      Bitmap: PBitmap;
      sControl: PControl;
      hRegion: HRGN;     
+     dw: DWORD;
+     Wnd: HWnd;
+
      procedure settransparent(Control: PControl);
      procedure setaeromode(Control: PControl);     
      function getbitmap: PBitmap;     
      procedure SetPicture(Value:HBITMAP);
+     procedure setalphatransparent;
+     procedure setalphapicture;     
    public
      _prop_Name:string;
      _prop_ControlManager:IControlManager;
@@ -66,59 +72,42 @@ function UpdateLayeredWindow( hwnd: HWND; hdcDst: HDC; pptDst: PPoint; psize: PS
                               blend: PBlendFunction; dwFlags: Dword): Boolean;
                               stdcall; external 'User32.dll' name 'UpdateLayeredWindow';
 
-procedure setalphatransparent(Control: PControl; FormTranspColor: TColor; AlphaBlendValue: byte);
+procedure THITransparentManager.setalphatransparent;
 var
-  dw: DWORD;
   dwFlags: Dword;
-  Wnd: HWnd;  
 begin
-  Wnd := Control.GetWindowHandle;
-  dw := GetWindowLong(Wnd, GWL_EXSTYLE);        
-
-  if (FormTranspColor = clNone) and (AlphaBlendValue = 255) then
-  begin
-    SetWindowLong(Wnd, GWL_EXSTYLE, dw and not WS_EX_LAYERED);
-    exit;
-  end
-  else if (FormTranspColor <> clNone) and (AlphaBlendValue <> 255) then
+  if (_prop_FormTranspColor <> clNone) and (_prop_AlphaBlendValue <> 255) then
     dwFlags := LWA_COLORKEY or LWA_ALPHA
-  else if (FormTranspColor <> clNone) and (AlphaBlendValue = 255) then
+  else if (_prop_FormTranspColor <> clNone) and (_prop_AlphaBlendValue = 255) then
     dwFlags := LWA_COLORKEY
   else
     dwFlags := LWA_ALPHA;     
 
+  dw := GetWindowLong(Wnd, GWL_EXSTYLE);
   if dw and WS_EX_LAYERED = 0 then
     SetWindowLong(Wnd, GWL_EXSTYLE, dw or WS_EX_LAYERED);
-  SetLayeredWindowAttributes(Wnd, Color2RGB(FormTranspColor), AlphaBlendValue, dwFlags);
+  SetLayeredWindowAttributes(Wnd, Color2RGB(_prop_FormTranspColor), _prop_AlphaBlendValue, dwFlags);
+  RedrawWindow(Wnd, nil, 0, RDW_ERASE or RDW_INVALIDATE or RDW_FRAME or RDW_ALLCHILDREN);
 end;
 
-procedure setalphapicture(Control: PControl; Bitmap: PBitmap; TransparentColor: TColor; AlphaBlendValue, TransparentType: byte);
+procedure THITransparentManager.setalphapicture;
 var
-  dw: DWORD;
   ptDst, ptSrc: TPoint;
   hdcDst, hdcSRC: HDC;
   size: TSize;
   blend: TBlendFunction;
   dwFlags: Dword;
-  Wnd: HWND;
 begin
-  Wnd := Control.GetWindowHandle;
+  dwFlags := ULW_ALPHA;
+
+//  if dw and WS_EX_LAYERED = 0 then
+    SetWindowLong(Wnd, GWL_EXSTYLE, dw or WS_EX_LAYERED);
 
   blend.BlendOp := AC_SRC_OVER;
   blend.BlendFlags := 0;
-  blend.SourceConstantAlpha := AlphaBlendValue;
+  blend.SourceConstantAlpha := _prop_AlphaBlendValue;
 
-  hdcDst := GetDC(0);
-  hdcSRC := CreateCompatibleDC(hdcDst);
-  SelectObject(hdcSRC, Bitmap.Handle);
-
-  ptDst := MakePoint(Control.Left, Control.Top);
-  ptSrc := MakePoint(0, 0);
-  size.cx := Bitmap.Width;
-  size.cy := Bitmap.Height;
-  
-  dwFlags := ULW_ALPHA;
-  case TransparentType of
+  case _prop_TransparentType of
     0: begin
          blend.AlphaFormat := 0;
          dwFlags := dwFlags or ULW_COLORKEY;      
@@ -126,10 +115,16 @@ begin
     1: blend.AlphaFormat := AC_SRC_NO_PREMULT_ALPHA;
   end;
 
-  dw := GetWindowLong(Wnd, GWL_EXSTYLE );
-  if dw and WS_EX_LAYERED = 0 then
-    SetWindowLong(Wnd, GWL_EXSTYLE, dw or WS_EX_LAYERED);
-  UpdateLayeredWindow(Wnd, hdcDst, @ptDst, @size, hdcSRC, @ptSrc, Color2RGB(TransparentColor), @blend, dwFlags);
+  hdcDst := GetDC(0);
+  hdcSRC := CreateCompatibleDC(hdcDst);
+  SelectObject(hdcSRC, Bitmap.Handle);
+
+  ptDst := MakePoint(sControl.Left, sControl.Top);
+  ptSrc := MakePoint(0, 0);
+  size.cx := Bitmap.Width;
+  size.cy := Bitmap.Height;
+
+  UpdateLayeredWindow(Wnd, hdcDst, @ptDst, @size, hdcSRC, @ptSrc, Color2RGB(_prop_TransparentColor), @blend, dwFlags);
   RedrawWindow(Wnd, nil, 0, RDW_ERASE or RDW_INVALIDATE or RDW_FRAME or RDW_ALLCHILDREN);  
   ReleaseDC(0, hdcDst);
   DeleteDC(hdcSRC);
@@ -150,38 +145,37 @@ begin
   if sControl = nil then exit;
   _prop_AlphaBlendValue := ToInteger(_Data);
   if (not Bitmap.Empty) and (_prop_TransparentMode = 1) then 
-    SetAlphaPicture(sControl, Bitmap, _prop_TransparentColor, _prop_AlphaBlendValue, _prop_TransparentType)
+    SetAlphaPicture
   else
-    SetAlphaTransparent(sControl, _prop_FormTranspColor, _prop_AlphaBlendValue);    
+    SetAlphaTransparent;    
 end;
      
 procedure THITransparentManager._work_doPicture;
 var
   bmp:PBitmap;
-  dw: Dword;
-  Wnd: HWND;
   form: THIMainForm;
 begin
   if sControl = nil then exit;
-  Wnd := sControl.GetWindowHandle; 
   bmp := ToBitmap(_Data);
 
   if bmp <> nil then
   begin
     BitMap.Assign(bmp);
-    dw := GetWindowLong(Wnd, GWL_EXSTYLE);
-    SetWindowLong(Wnd, GWL_EXSTYLE, dw and not WS_EX_LAYERED);
-    form := THIMainForm(sControl.Tag);
     if (not Bitmap.Empty) then
     begin
       if _prop_TransparentMode = 0 then
       begin
+        form := THIMainForm(sControl.Tag);
         form.Bitmap := Bitmap;
         DeleteObject(hRegion);
         hRegion := CreateCoolControl(Wnd, Bitmap, _prop_TransparentColor);
       end;
-      SetAlphaPicture(sControl, Bitmap, _prop_TransparentColor, _prop_AlphaBlendValue, _prop_TransparentType);
-      RedrawWindow(Wnd, nil, 0, RDW_ERASE or RDW_INVALIDATE or RDW_FRAME or RDW_ALLCHILDREN);
+      if IsNotInitBmp then
+      begin
+        SetWindowLong(Wnd, GWL_EXSTYLE, dw and not WS_EX_LAYERED);
+        IsNotInitBmp := false;
+      end;
+      SetAlphaPicture;
     end;  
   end;       
 end;
@@ -192,7 +186,8 @@ begin
   tm.settransparent := settransparent;
   tm.getbitmap := getbitmap;
   tm.setaeromode := setaeromode;
-  BitMap := NewBitmap(0,0);  
+  BitMap := NewBitmap(0,0);
+  IsNotInitBmp := true;    
 end;
 
 destructor THITransparentManager.Destroy;
@@ -209,26 +204,29 @@ end;
 
 procedure THITransparentManager.settransparent;
 var
-  Wnd: HWnd;
   form: THIMainForm;
 begin
   sControl := Control;
   Wnd := sControl.GetWindowHandle;
-  form := THIMainForm(sControl.Tag);
+  dw := GetWindowLong(Wnd, GWL_EXSTYLE);
+ 
   if (not Bitmap.Empty) then
   begin
+	IsNotInitBmp := false;
     if _prop_TransparentMode = 0 then
     begin
+      form := THIMainForm(sControl.Tag);
       form.Bitmap := Bitmap;
       DeleteObject(hRegion);
       hRegion := CreateCoolControl(Wnd, Bitmap, _prop_TransparentColor);
     end;
-    SetAlphaPicture(sControl, Bitmap, _prop_TransparentColor, _prop_AlphaBlendValue, _prop_TransparentType);
-    RedrawWindow(Wnd, nil, 0, RDW_ERASE or RDW_INVALIDATE or RDW_FRAME or RDW_ALLCHILDREN);
+    SetAlphaPicture;
   end  
   else
-    SetAlphaTransparent(sControl, _prop_FormTranspColor, _prop_AlphaBlendValue);    
+    SetAlphaTransparent;    
 end;
+
+// ---------------------- Aeromode -------------------------
 
 procedure THITransparentManager.setaeromode;
 const
