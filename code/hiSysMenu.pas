6f040f5d79d1ce81b2bdb9814c3ee442b2943a0b
nesco <nesco_sochi@mail.ru>
2016-08-16 21:25:35 +0000

    add: Method doPopupSysMenu

--- code/hiSysMenu.pas
+++ code/hiSysMenu.pas
@@ -11,6 +11,7 @@ type
     Style: Longint;
    public
     _data_Handle:THI_Event;
+    _event_onPopupSysMenu:THI_Event;
 
     procedure _work_doHideCloseButton(var _Data:TData; Index:word);
     procedure _work_doHideMinimizeButton(var _Data:TData; Index:word); 
@@ -20,6 +21,7 @@ type
     procedure _work_doShowMinimizeButton(var _Data:TData; Index:word);
     procedure _work_doShowMaximizeButton(var _Data:TData; Index:word);    
     procedure _work_doShowSYSMENU(var _Data:TData; Index:word);	
+    procedure _work_doPopupSysMenu(var _Data:TData; Index:word);    
   end;
 
 implementation
@@ -110,6 +112,22 @@ begin
       SetWindowLong(hHandle, GWL_STYLE, Style or WS_SYSMENU );
       DrawMenuBar (hHandle);
     end;
+end;
+
+procedure THISysMenu._work_doPopupSysMenu;
+var
+  pos: TPoint;
+  LItem: integer;
+  LMenu: HMENU;  
+begin
+  hHandle := ReadInteger(_Data,_data_Handle,0);
+  GetCursorPos(pos);
+  LMenu := GetSystemMenu(hHandle, false);      
+  with pos do
+    LItem := integer(TrackPopupMenu(LMenu, TPM_LEFTBUTTON or
+                                     TPM_RIGHTBUTTON or TPM_RETURNCMD,
+                                     x, y, 0, hHandle, nil));
+  _hi_onEvent(_event_onPopupSysMenu, LItem);
 end;
 
 end.
