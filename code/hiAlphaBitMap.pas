36a008dfe741e35ee74ff58397d8ef1e42c92df9
nesco <nesco_sochi@mail.ru>
2017-03-10 09:49:46 +0000

    add: Property PremultAlpha

--- code/hiAlphaBitMap.pas
+++ code/hiAlphaBitMap.pas
@@ -13,16 +13,19 @@ type
     _prop_HWidth  : integer;
     _prop_HHeight : integer;
     _prop_FillColor : TColor;    
+    _prop_PremultAlpha: boolean;
     _data_HWidth  : THI_Event;
     _data_HHeight : THI_Event;
     _data_FillColor : THI_Event;
     _event_onCreate : THI_Event;
     _event_onLoad : THI_Event;    
+
     constructor Create;
     destructor Destroy; override;
     procedure _work_doLoad(var _Data:TData; Index:word);
     procedure _work_doCreate(var _Data:TData; Index:word);
-    procedure _work_doClear(var _Data:TData; Index:word);
+    procedure _work_doClear(var _Data:TData; Index:word);    
+    procedure _work_PremultAlpha(var _Data:TData; Index:word);
     procedure _var_Bitmap(var _Data:TData; Index:word);
     procedure _var_Width(var _Data:TData; Index:word);
     procedure _var_Height(var _Data:TData; Index:word);
@@ -34,13 +37,37 @@ implementation
 constructor THIAlphaBitmap.Create;
 begin
   inherited;
-  Bmp := NewBitmap(0,0);
+  Bmp := NewBitmap(0, 0);
 end;
 
 destructor THIAlphaBitmap.Destroy;
 begin
   Bmp.free;
   inherited;
+end;
+
+procedure PremultAlphaTransparent(BMP: PBitmap; TransparentColor: TColor; DeleteAlpha: Boolean);
+var
+  i: Integer;
+  q: PRGBQuad;
+  Red, Green, Blue: byte;
+begin
+  Red := GetRValue(TransparentColor);
+  Green := GetGValue(TransparentColor);
+  Blue := GetBValue(TransparentColor);
+  q := BMP.ScanLine[BMP.Height - 1];
+
+  for i:=0 to BMP.Height * BMP.Width - 1 do
+  begin
+    if (q.rgbRed = Red) and (q.rgbGreen = Green) and (q.rgbBlue = Blue) then
+      q.rgbReserved := 0
+    else if DeleteAlpha then
+      q.rgbReserved := 255;
+    q.rgbBlue := q.rgbBlue * q.rgbReserved Shr 8;
+    q.rgbGreen := q.rgbGreen * q.rgbReserved Shr 8;
+    q.rgbRed := q.rgbRed * q.rgbReserved Shr 8;
+    Inc(q);
+  end;
 end;
 
 procedure THIAlphaBitmap._work_doLoad;
@@ -50,24 +77,33 @@ begin
   if tmp <> nil then
     Bmp.Assign(tmp);
   if bmp.PixelFormat <> pf32bit then bmp.PixelFormat := pf32bit;
+  if _prop_PremultAlpha then PremultAlphaTransparent(bmp, clNone, true);
   _hi_onEvent(_event_onLoad, bmp);    
 end;
 
 procedure THIAlphaBitmap._work_doCreate;
+var
+  br: HBRUSH;
 begin
   With Bmp{$ifndef F_P}^{$endif} do
   begin
     Clear;
     Width := ReadInteger(_Data, _data_HWidth, _prop_HWidth);
     Height := ReadInteger(_Data, _data_HHeight, _prop_HHeight);
+    PixelFormat := pf32bit;
     if not Bmp.Empty then
     begin
-      Canvas.Brush.Color := ReadInteger(_Data, _data_FillColor, _prop_FillColor);
-      Canvas.Brush.BrushStyle := bsSolid;
-      Canvas.FillRect(MakeRect(0,0,Bmp.Width,Bmp.Height));
+      br := CreateSolidBrush(ReadInteger(_Data, _data_FillColor, _prop_FillColor));
+      SelectObject(Canvas.Handle, br);
+      Rectangle(Canvas.Handle, 0, 0, Width, Height);
+      DeleteObject(br);
+//      Canvas.Brush.Color := ReadInteger(_Data, _data_FillColor, _prop_FillColor);
+//      Canvas.Brush.BrushStyle := bsSolid;
+//      Canvas.FillRect(MakeRect(0,0,Bmp.Width,Bmp.Height));
     end;
   end;
-  if bmp.PixelFormat <> pf32bit then bmp.PixelFormat := pf32bit;
+//  bmp.PixelFormat := pf32bit;
+  if _prop_PremultAlpha then PremultAlphaTransparent(bmp, clNone, true);
   _hi_onEvent(_event_onCreate, bmp);
 end;
 
@@ -86,6 +122,7 @@ begin
   if Value = 0 then exit;
   bmp.handle := CopyImage(Value, IMAGE_BITMAP, 0, 0, LR_CREATEDIBSECTION);
   if bmp.PixelFormat <> pf32bit then bmp.PixelFormat := pf32bit;
+  if _prop_PremultAlpha then PremultAlphaTransparent(bmp, clNone, true);
 end;
 
 procedure THIAlphaBitmap._var_Width;
@@ -96,6 +133,11 @@ end;
 procedure THIAlphaBitmap._var_Height;
 begin
   dtInteger(_Data,Bmp.Height);
+end;
+
+procedure THIAlphaBitmap._work_PremultAlpha;
+begin
+  _prop_PremultAlpha := ReadBool(_Data);
 end;
 
 end.
