90a2687dfabdd0298ea2840c2792d83787f23348
nesco <nesco_sochi@mail.ru>
2017-05-14 16:03:40 +0000

    add: New Method FileLong

--- code/hiFilePartElm.pas
+++ code/hiFilePartElm.pas
@@ -2,7 +2,7 @@ unit hiFilePartElm;
 
 interface
 
-uses Kol, Share, Debug;
+uses Windows, Kol, Share, Debug;
 
 type
   THIFilePartElm = class(TDebug)
@@ -21,12 +21,22 @@ type
     procedure _work_doPart4(var _Data: TData; Index: word);  // ext name WOPoint    
     procedure _work_doPart5(var _Data: TData; Index: word);  // short name
     procedure _work_doPart6(var _Data: TData; Index: word);  // path name WOExt
+    procedure _work_doPart7(var _Data: TData; Index: word);  // long name   
     procedure _var_Part(var _Data: TData; Index: word);
   end;
 
 implementation
 
 uses hiStr_Enum;
+
+function GetLongPathName(lpszShortName: LPCTSTR; lpszLongName: LPTSTR;
+         cchBuffer: DWORD): DWORD; stdcall; external 'kernel32.dll' name 'GetLongPathNameA';
+
+function ShortToLongFileName(FileName: string): string;
+begin
+  SetLength(Result, MAX_PATH + 1);
+  SetLength(Result, GetLongPathName(PChar(FileName), @Result[1], MAX_PATH));
+end;
 
 procedure THIFilePartElm._work_doPart0;  // path name
 begin
@@ -107,6 +117,14 @@ begin
    _hi_CreateEvent(_Data, @_event_onPart, FPart);
 end;
 //=========================================
+
+procedure THIFilePartElm._work_doPart7;  // long name
+begin
+   FPart := ReadString(_Data, _data_FileName, '');
+   if FPart = '' then exit;
+   FPart := ShortToLongFileName(FPart);
+   _hi_CreateEvent(_Data, @_event_onPart, FPart);
+end;
 
 procedure THIFilePartElm._var_Part;
 begin
