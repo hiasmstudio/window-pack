727bbde9683bb211e23754cc9f1a155084b01112
nesco <nesco_sochi@mail.ru>
2017-09-19 09:32:05 +0000

    add: New Proprty RespondToEnter and Support Cansel Button

--- code/hiButton.pas
+++ code/hiButton.pas
@@ -2,7 +2,7 @@ unit hiButton;
 
 interface
 
-uses Windows,Kol,Share,Win;
+uses Windows,Messages,Kol,Share,Win;
 
 {$I share.inc}
 
@@ -14,21 +14,36 @@ type
      _event_onClick:THI_Event;
      _prop_Data:TData;
      _prop_Caption:string;
-     _prop_DefaultBtn:boolean;
-
+     _prop_DefaultBtn:byte; // изменен тип с boolean на byte
+     _prop_RespondToEnter:boolean;
+     
      procedure Init; override;
      procedure _work_doCaption(var _Data:TData; Index:word);
  end;
 
 implementation
 
+function WndProcBtnReturnClick( Self_: PControl; var Msg: TMsg; var Rslt: Integer ): Boolean;
+begin
+  Result := FALSE;
+  if ((Msg.message = WM_KEYDOWN) or (Msg.message = WM_KEYUP) or
+      (Msg.message = WM_CHAR)) and (Msg.wParam = 13) then
+    Msg.wParam := 32;
+end;
+
 procedure THIButton.Init;
 begin
    Control := NewButton(FParent,_prop_Caption);
    Control.OnClick := _OnClick;
-   Control.DefaultBtn := _prop_DefaultBtn;
+//   Control.DefaultBtn := _prop_DefaultBtn;
    Control.Style := Control.Style or BS_MULTILINE;
+   if (_prop_RespondToEnter) and (_prop_DefaultBtn = 1) then
+     Control.AttachProc( WndProcBtnReturnClick );
    inherited;
+   Case _prop_DefaultBtn of
+     0: Control.DefaultBtn := true;
+     2: Control.CancelBtn  := true;
+   end;
 end;
 
 procedure THIButton._work_doCaption;
