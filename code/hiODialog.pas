3ed5e01cf27e55b6868b27ebdcd53a3555c105a8
nesco <nesco_sochi@mail.ru>
2017-10-13 20:34:00 +0000

    add: Properties Options

--- code/hiODialog.pas
+++ code/hiODialog.pas
@@ -2,29 +2,34 @@ unit hiODialog;
 
 interface
 
-uses Kol,Share,Debug;
+uses Kol, Share, Debug;
 
 type
   THIODialog = class(TDebug)
-   private
-    FOpenDialog:POpenSaveDialog;
-   public
-    _prop_StartDir:string;
-    _prop_Filter:string;
-    _prop_Title:string;
-    _prop_FileName:string;
-    _prop_Select:byte;
-    _event_onExecute:THI_Event;
-    _event_onCancel:THI_Event;
-    _data_FileName:THI_Event;
-    _data_StartDir:THI_Event;
+    private
+      FOpenDialog: POpenSaveDialog;
+    public
+      _prop_StartDir: string;
+      _prop_Filter: string;
+      _prop_Title: string;
+      _prop_FileName: string;
+      _prop_Select: Byte;
+      
+      // By NetSpirit
+      _prop_NoChangeDir: Byte;
+      _prop_FileMustExists: Byte;
+      
+      _event_onExecute: THI_Event;
+      _event_onCancel: THI_Event;
+      _data_FileName: THI_Event;
+      _data_StartDir: THI_Event;
 
-    destructor Destroy; override;
-    procedure _work_doExecute0(var _Data:TData; Index:word);
-    procedure _work_doExecute1(var _Data:TData; Index:word);
-    procedure _work_doStartDir(var _Data:TData; Index:word);
-    procedure _work_doFileName(var _Data:TData; Index:word);
-    procedure _work_doFilter(var _Data:TData; Index:word);
+      destructor Destroy; override;
+      procedure _work_doExecute0(var _Data: TData; Index: Word);
+      procedure _work_doExecute1(var _Data: TData; Index: Word);
+      procedure _work_doStartDir(var _Data: TData; Index: Word);
+      procedure _work_doFileName(var _Data: TData; Index: Word);
+      procedure _work_doFilter(var _Data: TData; Index: Word);
   end;
 
 implementation
@@ -36,63 +41,79 @@ begin
 end;
 
 procedure THIODialog._work_doExecute0;
+var
+  Opts: TOpenSaveOptions;
 begin
   if not Assigned(FOpenDialog) then
-   begin
-    FOpenDialog := NewOpenSaveDialog(_prop_Title,_prop_StartDir,[OSNoValidate]);
+  begin
+    // By NetSpirit
+    Opts := DefOpenSaveDlgOptions;
+    if _prop_NoChangeDir = 1 then Opts := Opts + [OSNoChangedir];
+    if _prop_FileMustExists = 0 then Opts := Opts - [OSFileMustExist];
+    FOpenDialog := NewOpenSaveDialog(_prop_Title, _prop_StartDir, Opts);
+    
     FOpenDialog.WndOwner := ReadHandle;
     FOpenDialog.Filter := _prop_Filter;
-   end;
+  end;
 
-  FOpenDialog.Filename := ReadString(_Data,_data_FileName,_prop_FileName);
+  FOpenDialog.Filename := ReadString(_Data, _data_FileName, _prop_FileName);
   if FOpenDialog.Execute then
-    _hi_CreateEvent(_Data,@_event_onExecute,FOpenDialog.Filename)
-  else _hi_CreateEvent(_Data,@_event_onCancel);
+    _hi_CreateEvent(_Data, @_event_onExecute, FOpenDialog.Filename)
+  else
+    _hi_CreateEvent(_Data, @_event_onCancel);
 end;
 
 procedure THIODialog._work_doExecute1;
 var
-  lst:PStrList;
-  i:word;
-  s:string;
+  Lst: PStrList;
+  I: Word;
+  S: string;
+  Opts: TOpenSaveOptions;
 begin
   if not Assigned(FOpenDialog) then
-    begin
-      FOpenDialog := NewOpenSaveDialog(_prop_Title,_prop_StartDir,[OSAllowMultiSelect,OSNoValidate]);
-      FOpenDialog.WndOwner := ReadHandle;
-      FOpenDialog.Filter := _prop_Filter;
-    end;
+  begin
+    // By NetSpirit
+    Opts := DefOpenSaveDlgOptions + [OSAllowMultiSelect];
+    if _prop_NoChangeDir = 1 then Opts := Opts + [OSNoChangedir];
+    if _prop_FileMustExists = 0 then Opts := Opts - [OSFileMustExist];
+    FOpenDialog := NewOpenSaveDialog(_prop_Title, _prop_StartDir, Opts);
+    
+    FOpenDialog.WndOwner := ReadHandle;
+    FOpenDialog.Filter := _prop_Filter;
+  end;
 
-  FOpenDialog.Filename := ReadString(_Data,_data_FileName,_prop_FileName);
+  FOpenDialog.FileName := ReadString(_Data, _data_FileName, _prop_FileName);
+  
   if FOpenDialog.Execute then
-   begin
-    lst := NewStrList;
-    lst.Text := FOpenDialog.filename;
-    if lst.Count > 0 then
-      s := lst.items[0];
-    if lst.Count = 1 then
-      _hi_CreateEvent(_Data,@_event_onExecute,s)
+  begin
+    Lst := NewStrList;
+    Lst.Text := FOpenDialog.FileName;
+    if Lst.Count > 0 then
+      S := Lst.Items[0];
+    if Lst.Count = 1 then
+      _hi_CreateEvent(_Data, @_event_onExecute, S)
     else
-     begin
-      s := s + '\';
-      for i := 1 to lst.Count-1 do
-        _hi_OnEvent(_event_onExecute, s + lst.items[i]);
-      FOpenDialog.Filename := '';
-     end;
-    lst.Free;
-   end
-  else _hi_CreateEvent(_Data,@_event_onCancel);
+    begin
+      S := S + '\';
+      for I := 1 to Lst.Count - 1 do
+        _hi_OnEvent(_event_onExecute, S + Lst.Items[I]);
+      FOpenDialog.FileName := '';
+    end;
+    Lst.Free;
+  end
+  else
+    _hi_CreateEvent(_Data, @_event_onCancel);
 end;
 
 procedure THIODialog._work_doStartDir;
 begin
-  _prop_StartDir := ReadString(_Data,_data_StartDir,'');
+  _prop_StartDir := ReadString(_Data, _data_StartDir, '');
   if Assigned(FOpenDialog) then FOpenDialog.InitialDir := _prop_StartDir;
 end;
 
 procedure THIODialog._work_doFileName;
 begin
-  _prop_FileName := ReadString(_Data,_data_FileName,'');
+  _prop_FileName := ReadString(_Data, _data_FileName, '');
 end;
 
 procedure THIODialog._work_doFilter;
--- code/hiSDialog.pas
+++ code/hiSDialog.pas
@@ -2,27 +2,33 @@ unit hiSDialog;
 
 interface
 
-uses Kol,Share,Debug;
+uses Kol, Share, Debug;
 
 type
   THISDialog = class(TDebug)
-   private
-    FSaveDialog:POpenSaveDialog;
-   public
-    _prop_StartDir:string;
-    _prop_Filter:string;
-    _prop_Title:string;
-    _prop_FileName:string;
-    _event_onExecute:THI_Event;
-    _event_onCancel:THI_Event;
-    _data_FileName:THI_Event;
-    _data_StartDir:THI_Event;
+    private
+      FSaveDialog: POpenSaveDialog;
+    public
+      _prop_StartDir: string;
+      _prop_Filter: string;
+      _prop_Title: string;
+      _prop_FileName: string;
+      
+      // By NetSpirit
+      _prop_NoChangeDir: Byte;
+      _prop_PathMustExists: Byte;
+      _prop_OverwritePrompt: Byte;
+      
+      _event_onExecute: THI_Event;
+      _event_onCancel: THI_Event;
+      _data_FileName: THI_Event;
+      _data_StartDir: THI_Event;
 
-    destructor Destroy; override;
-    procedure _work_doExecute(var _Data:TData; Index:word);
-    procedure _work_doStartDir(var _Data:TData; Index:word);
-    procedure _work_doFileName(var _Data:TData; Index:word);
-    procedure _work_doFilter(var _Data:TData; Index:word);    
+      destructor Destroy; override;
+      procedure _work_doExecute(var _Data: TData; Index: Word);
+      procedure _work_doStartDir(var _Data: TData; Index: Word);
+      procedure _work_doFileName(var _Data: TData; Index: Word);
+      procedure _work_doFilter(var _Data: TData; Index: Word);    
   end;
 
 implementation
@@ -34,30 +40,39 @@ begin
 end;
 
 procedure THISDialog._work_doExecute;
+var
+  Opts: TOpenSaveOptions;
 begin
   if not Assigned(FSaveDialog) then
-   begin
-    FSaveDialog := NewOpenSaveDialog(_prop_Title,_prop_StartDir,[]);
+  begin
+    // By NetSpirit
+    Opts := DefOpenSaveDlgOptions;
+    if _prop_NoChangeDir = 1 then Opts:= Opts + [OSNoChangedir];
+    if _prop_PathMustExists = 0 then Opts := Opts - [OSPathMustExist, OSFileMustExist];
+    if _prop_OverwritePrompt = 0 then Opts := Opts - [OSOverwritePrompt];
+    FSaveDialog := NewOpenSaveDialog(_prop_Title,_prop_StartDir, Opts);
+    
     FSaveDialog.Filter := _prop_Filter;
-    FSaveDialog.OpenDialog := false;
+    FSaveDialog.OpenDialog := False;
     FSaveDialog.WndOwner := ReadHandle;
-   end;
+  end;
 
-  FSaveDialog.Filename := ReadString(_Data,_data_FileName,_prop_FileName);
+  FSaveDialog.FileName := ReadString(_Data, _data_FileName, _prop_FileName);
   if FSaveDialog.Execute then
-    _hi_CreateEvent(_Data,@_event_onExecute,FSaveDialog.filename)
-  else _hi_CreateEvent(_Data,@_event_onCancel);
+    _hi_CreateEvent(_Data, @_event_onExecute, FSaveDialog.FileName)
+  else
+    _hi_CreateEvent(_Data, @_event_onCancel);
 end;
 
 procedure THISDialog._work_doStartDir;
 begin
-  _prop_StartDir := ReadString(_Data,_data_StartDir,'');
+  _prop_StartDir := ReadString(_Data, _data_StartDir, '');
   if Assigned(FSaveDialog) then FSaveDialog.InitialDir := _prop_StartDir;
 end;
 
 procedure THISDialog._work_doFileName;
 begin
-  _prop_FileName := ReadString(_Data,_data_FileName,'');
+  _prop_FileName := ReadString(_Data, _data_FileName, '');
 end;
 
 procedure THISDialog._work_doFilter;
